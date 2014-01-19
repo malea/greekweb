8bd14e0143eaca9f82966cdea73c37efa8fdede1
Malea Grubb <maleangrubb@gmail.com>
2014-01-18 19:12:27 -0500

    Add Bootstrap

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def root():
 def login():
     return '<a href="/">Return home</a>'
 
+@app.route('/js/<filename>')
+def js(filename):
+  return bottle.static_file(filename, root='static/js')
+
+@app.route('/css/<filename>')
+def css(filename):
+  return bottle.static_file(filename, root='static/css')
+
+@app.route('/fonts/<filename>')
+def fonts(filename):
+  return bottle.static_file(filename, root='static/fonts')
+
 if __name__ == '__main__':
     bottle.run(app, host='localhost', port=8080, debug=True, reloader=True)
